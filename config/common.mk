e429662c8f0a49b5e6b3d0b6bfdcd8f3a5ef0816
Lily <owo@uwuli.li>
2022-06-08 18:33:29 +0200

    Update common.mk

--- config/common.mk
+++ config/common.mk
@@ -214,4 +214,3 @@ include vendor/lineage/config/version.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
--include vendor/lineage/config/apps.mk
