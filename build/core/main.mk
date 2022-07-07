4b9a057494084d1f0498305cba48cee468e125b8
Lily <lz@girlc.at>
2022-07-08 00:18:51 +0200

    Update main.mk

--- build/core/main.mk
+++ build/core/main.mk
@@ -2,9 +2,9 @@
 include $(TOPDIR)vendor/lineage/build/core/main_version.mk
 
 #ADDITIONAL_SYSTEM_PROPERTIES += \
+    ro.build.stock_fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
 #    ro.build.tags=release-keys \
 #    ro.build.flavor=$(PRODUCT_NAME)-user \
-    ro.build.stock_fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
 
 # Description needs special treatment because it contains spaces
 PRIVATE_BUILD_DESC := $(PRODUCT_OVERRIDE_DESC)
