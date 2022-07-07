3d4a2d41ed920f4ea3a692e343579d0b91dd15b7
Lily <lz@girlc.at>
2022-07-08 00:19:05 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAA

--- build/core/main.mk
+++ build/core/main.mk
@@ -1,7 +1,7 @@
 # Include LineageOS versions
 include $(TOPDIR)vendor/lineage/build/core/main_version.mk
 
-#ADDITIONAL_SYSTEM_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.stock_fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
 #    ro.build.tags=release-keys \
 #    ro.build.flavor=$(PRODUCT_NAME)-user \
